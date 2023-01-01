96d0c37f15e023fc656b90008c440771ec4b6f73
limtaewon <tagkdj1@naver.com>
2023-01-01 16:03:33 +0900

    Feat : 파일생성
    CI/CD를 위한 파일생성

---
+++ src/main/java/com/bookpub/bookpubfront/BookpubFrontApplication.java
@@ -0,0 +1,13 @@
+package com.bookpub.bookpubfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpubFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpubFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookpub/bookpubfront/BookpubFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookpub.bookpubfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookpubFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
