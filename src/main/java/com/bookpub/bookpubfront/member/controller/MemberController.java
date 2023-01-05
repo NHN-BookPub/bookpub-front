3a89c878341c16257ba0b443f78a390234cbb06d
limtaewon <tagkdj1@gmail.com>
2023-01-05 21:22:22 +0900

    [Feat] front-gateway-auth 서버 연결
    간단한 뷰 작성 후 버튼 클릭시 auth서버에서 임의의 값 받아오기

--- src/main/java/com/bookpub/bookpubfront/member/controller/MemberController.java
+++ src/main/java/com/bookpub/bookpubfront/member/controller/MemberController.java
@@ -3,14 +3,13 @@ package com.bookpub.bookpubfront.member.controller;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
 
 /**
  * 멤버 api 컨트롤러.
@@ -31,27 +30,23 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public void loginCheck(@RequestParam String id,
-                           @RequestParam String pwd) {
+    public String loginCheck(@RequestParam String id,
+                           @RequestParam String pwd,
+                           Model model) {
         RestTemplate restTemplate = new RestTemplate();
-        UserDTO user = new UserDTO(id,pwd);
-        HttpHeaders headers = new HttpHeaders();
-        HttpEntity<String> httpEntity = new HttpEntity<>(new HttpHeaders());
 
-        restTemplate.exchange(
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> exchange = restTemplate.exchange(
                 "http://localhost:7070/auth/token",
                 HttpMethod.POST,
                 httpEntity,
                 String.class
-                );
-    }
+        );
 
+        model.addAttribute("token", exchange.getBody());
 
-    @AllArgsConstructor
-    @Getter
-    @ToString
-    private static class UserDTO {
-        private String id;
-        private String pwd;
+        return "tokenPage";
     }
 }
