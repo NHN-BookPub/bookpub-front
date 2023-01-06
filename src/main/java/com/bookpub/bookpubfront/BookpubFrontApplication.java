e84ae80b713e8dcbe4e6012a4c8615beebe44df2
HoFe-U <hochul-y@naver.com>
2023-01-06 10:06:53 +0900

    [Chore] : 기본개발환경설정

--- src/main/java/com/bookpub/bookpubfront/BookpubFrontApplication.java
+++ src/main/java/com/bookpub/bookpubfront/BookpubFrontApplication.java
@@ -2,8 +2,10 @@ package com.bookpub.bookpubfront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BookpubFrontApplication {
 
     public static void main(String[] args) {
