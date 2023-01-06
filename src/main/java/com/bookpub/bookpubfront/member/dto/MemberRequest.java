605adf5b57e2894d1596d4c0aaea227e188f8c59
limtaewon <tagkdj1@gmail.com>
2023-01-06 15:13:59 +0900

    [Refactor] 회원가입 폼 수정
    아이디, 닉네임, 패스워드 조건에 맞게 js validation

--- src/main/java/com/bookpub/bookpubfront/member/dto/MemberRequest.java
+++ src/main/java/com/bookpub/bookpubfront/member/dto/MemberRequest.java
@@ -1,10 +1,13 @@
 package com.bookpub.bookpubfront.member.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * Some description here
+ * member Request DTO개체
  *
  * @author : 임태원
  * @since : 1.0
@@ -12,5 +15,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class MemberRequest {
+    @NotBlank
+    @Size(min = 5, max = 20)
+    private String ID;
+
+    @NotBlank
+    @Size(min = 8, max = 20)
+    private String pwd;
+
 
 }
