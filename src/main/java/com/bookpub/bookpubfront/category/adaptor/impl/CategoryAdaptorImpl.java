ec251278a4f74c17ed38c020521c690bad6ae7e0
seohyeon <kgustjgus@naver.com>
2023-01-10 09:47:49 +0900

    [Feat] : api 서버와 연결 되는 CategoryAdaptor 추가

---
+++ src/main/java/com/bookpub/bookpubfront/category/adaptor/impl/CategoryAdaptorImpl.java
@@ -0,0 +1,126 @@
+package com.bookpub.bookpubfront.category.adaptor.impl;
+
+import com.bookpub.bookpubfront.category.adaptor.CategoryAdaptor;
+import com.bookpub.bookpubfront.category.dto.request.CreateCategoryRequestDto;
+import com.bookpub.bookpubfront.category.dto.request.ModifyCategoryRequestDto;
+import com.bookpub.bookpubfront.category.dto.response.GetCategoryResponseDto;
+import com.bookpub.bookpubfront.config.GateWayConfig;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * shop 과 연동을 위한 CategoryAdaptor 구현.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+@Component
+@RequiredArgsConstructor
+public class CategoryAdaptorImpl implements CategoryAdaptor {
+
+    private final GateWayConfig gateWayConfig;
+    private final ObjectMapper objectMapper;
+    private final RestTemplate restTemplate;
+
+    private static final String CATEGORY_URI = "/api/categories";
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void requestAddCategory(CreateCategoryRequestDto createCategoryRequestDto)
+            throws JsonProcessingException {
+
+        String request = objectMapper.writeValueAsString(createCategoryRequestDto);
+
+        String url = gateWayConfig.getGatewayUrl() + CATEGORY_URI;
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(request, makeHttpHeaders());
+
+        ResponseEntity<Void> response = restTemplate.exchange(url, HttpMethod.POST, httpEntity,
+                Void.class);
+
+        checkError(response);
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void requestModifyCategory(ModifyCategoryRequestDto modifyCategoryRequestDto)
+            throws JsonProcessingException {
+
+        String request = objectMapper.writeValueAsString(modifyCategoryRequestDto);
+        String url = gateWayConfig.getGatewayUrl() + CATEGORY_URI;
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(request, makeHttpHeaders());
+
+        ResponseEntity<Void> response = restTemplate.exchange(url, HttpMethod.PUT, httpEntity,
+                Void.class);
+
+        checkError(response);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<GetCategoryResponseDto> requestCategoryList() {
+
+        String url = gateWayConfig.getGatewayUrl() + CATEGORY_URI;
+
+        ResponseEntity<List<GetCategoryResponseDto>> response = restTemplate.exchange(url,
+                HttpMethod.GET,
+                new HttpEntity<>(makeHttpHeaders()),
+                new ParameterizedTypeReference<>() {
+                });
+
+        checkError(response);
+        return response.getBody();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GetCategoryResponseDto requestCategory(Integer categoryNo) {
+
+        String url = gateWayConfig.getGatewayUrl() + CATEGORY_URI + "/" + categoryNo;
+
+        ResponseEntity<GetCategoryResponseDto> response = restTemplate.exchange(url, HttpMethod.GET,
+                new HttpEntity<>(makeHttpHeaders()),
+                new ParameterizedTypeReference<>() {
+                });
+
+        checkError(response);
+        return response.getBody();
+    }
+
+    private static <T> void checkError(ResponseEntity<T> response) {
+        HttpStatus status = response.getStatusCode();
+
+        if (status.is4xxClientError() || status.is5xxServerError()) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static HttpHeaders makeHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        return headers;
+    }
+}
