4b0f6f8f9bb4d9624a1b5c7770c6eb705b045b7a
hobby3530 <jungy3530@gmail.com>
2023-01-10 20:25:59 +0900

    [Refactor] CouponPolicy 관련 Dto 수정
    필요없는 생성자 어노테이션 삭제

--- src/main/java/com/bookpub/bookpubfront/couponpolicy/dto/response/GetCouponPolicyResponseDto.java
+++ src/main/java/com/bookpub/bookpubfront/couponpolicy/dto/response/GetCouponPolicyResponseDto.java
@@ -1,8 +1,6 @@
 package com.bookpub.bookpubfront.couponpolicy.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 /**
  * 쿠폰정책을 조회하기 위해 필요한 정보들을 담을 Dto.
@@ -11,8 +9,6 @@ import lombok.NoArgsConstructor;
  * @since : 1.0
  **/
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class GetCouponPolicyResponseDto {
     private Integer policyNo;
     private boolean policyFixed;
