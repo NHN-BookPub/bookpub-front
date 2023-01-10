b534af26f43d7229196def06e3bdd3e89e4c5d30
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-10 09:57:59 +0900

    [Refactor] 사용하지 않는 변수 제거

--- src/main/java/com/bookpub/bookpubfront/tag/controller/TagController.java
+++ src/main/java/com/bookpub/bookpubfront/tag/controller/TagController.java
@@ -1,15 +1,12 @@
 package com.bookpub.bookpubfront.tag.controller;
 
-
 import com.bookpub.bookpubfront.tag.dto.AddTagRequestDto;
 import com.bookpub.bookpubfront.tag.dto.GetTagResponseDto;
 import com.bookpub.bookpubfront.tag.dto.ModifyTagRequestDto;
 import com.bookpub.bookpubfront.tag.service.TagService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,21 +29,17 @@ public class TagController {
 
     private final TagService tagService;
 
-    @Value("${book-pub.gateway}")
-    private String gatewayUrl;
 
     /**
      * 관레자 페이지에서 태그 목록을 조회해서 보여주는 메서드.
      *
      * @param model view 로 정보를 보내는 request
      * @return 태그 목록
-     * @throws JsonProcessingException Json 파싱 중 생기는 오류
      */
     @GetMapping("/tagList")
-    public String tagList(Model model) throws JsonProcessingException {
+    public String tagList(Model model) {
         List<GetTagResponseDto> tags = tagService.findAllTags();
 
-        model.addAttribute("gatewayUrl", gatewayUrl);
         model.addAttribute("tags", tags);
 
         return "admin/tag/tagIndex";
