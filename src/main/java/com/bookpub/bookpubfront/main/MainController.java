b7111d4e086cadb0f7ce83bb7478d2797905de45
seohyeon <kgustjgus@naver.com>
2023-01-12 02:24:23 +0900

    [Refactor] : MainController 에 카테고리 조회를 위해 model 추가

--- src/main/java/com/bookpub/bookpubfront/main/MainController.java
+++ src/main/java/com/bookpub/bookpubfront/main/MainController.java
@@ -1,6 +1,11 @@
 package com.bookpub.bookpubfront.main;
 
+import com.bookpub.bookpubfront.category.dto.response.GetParentCategoryWithChildrenResponseDto;
+import com.bookpub.bookpubfront.category.service.CategoryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -11,10 +16,17 @@ import org.springframework.web.bind.annotation.GetMapping;
  **/
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final CategoryService categoryService;
+
     @GetMapping("/")
-    public String mainView() {
+    public String mainView(Model model) {
+        List<GetParentCategoryWithChildrenResponseDto> parentCategoryWithChildren =
+                categoryService.getParentCategoryWithChildren();
+        model.addAttribute("category", parentCategoryWithChildren);
+
         return "main/index";
     }
 }
