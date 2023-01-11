3ed538a1c7ad32770b0e4c47a24ea4286c82544d
seohyeon <kgustjgus@naver.com>
2023-01-12 01:23:41 +0900

    [Feat] : 메인페이지에서 상위,하위 카테고리 조회를 위한 dto 추가.

---
+++ src/main/java/com/bookpub/bookpubfront/category/dto/response/GetChildCategoryResponseDto.java
@@ -0,0 +1,19 @@
+package com.bookpub.bookpubfront.category.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 메인페이지에서 하위 카테고리 정보를 반환하는 dto 입니다.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+@Getter
+@AllArgsConstructor
+public class GetChildCategoryResponseDto {
+
+    private Integer categoryNo;
+    private String categoryName;
+
+}
