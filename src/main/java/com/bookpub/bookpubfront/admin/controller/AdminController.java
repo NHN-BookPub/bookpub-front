1af0ff079b2d5e190a4e41987c00f6b18fd09982
limtaewon <tagkdj1@gmail.com>
2023-01-11 16:02:16 +0900

    [Refactor] 간단 로직 수정

--- src/main/java/com/bookpub/bookpubfront/admin/controller/AdminController.java
+++ src/main/java/com/bookpub/bookpubfront/admin/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.bookpub.bookpubfront.admin.controller;
 
+import java.io.File;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
