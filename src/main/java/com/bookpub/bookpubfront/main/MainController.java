321ddcd1d1fda901640e0a393a701072d766db82
limtaewon <tagkdj1@gmail.com>
2023-01-15 12:30:58 +0900

    [Refactor] 로그인 임시 기능
    로그인한 사용자의 정보를 불러와서 view에 정보 전달 로직 추가
    추후 추가변경 있을 예정

--- src/main/java/com/bookpub/bookpubfront/main/MainController.java
+++ src/main/java/com/bookpub/bookpubfront/main/MainController.java
@@ -1,6 +1,12 @@
 package com.bookpub.bookpubfront.main;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -11,10 +17,22 @@ import org.springframework.web.bind.annotation.GetMapping;
  **/
 
 @Controller
+@Slf4j
+@RequiredArgsConstructor
 public class MainController {
+    private final RedisTemplate<String,String> redisTemplate;
 
     @GetMapping("/")
-    public String mainView() {
+    public String mainView(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String memberUUID = (String) authentication.getPrincipal();
+        String sessionId = (String) authentication.getCredentials();
+        String userId
+                = (String) redisTemplate.opsForHash().get(memberUUID, sessionId);
+
+        model.addAttribute("userId", userId);
+
         return "main/index";
     }
 }
