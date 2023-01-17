4d62fde416e7effc155ccfe6910cadeb0efaa45a
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-17 11:10:44 +0900

    [Feat] 상품 등록, 수정, 조회를 위한 DTO 구현

---
+++ src/main/java/com/bookpub/bookpubfront/product/dto/response/GetProductListResponseDto.java
@@ -0,0 +1,25 @@
+package com.bookpub.bookpubfront.product.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 상품 목록 페이지에서 보여줄 정보를 담는 DTO.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetProductListResponseDto {
+    private Long productNo;
+    private String title;
+    private Integer productStock;
+    private Long salesPrice;
+    private Integer saleRate;
+    private Long productPrice;
+    private boolean deleted;
+}
+
