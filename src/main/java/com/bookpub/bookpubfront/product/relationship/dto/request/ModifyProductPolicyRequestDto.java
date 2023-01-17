d4a1dab781d719ebd546a1090686e4b565528daa
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-17 11:11:14 +0900

    [Feat] 상품 정책 수정을 위한 DTO 구현

---
+++ src/main/java/com/bookpub/bookpubfront/product/relationship/dto/request/ModifyProductPolicyRequestDto.java
@@ -0,0 +1,22 @@
+package com.bookpub.bookpubfront.product.relationship.dto.request;
+
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * 상품 정책 수정을 위한 DTO.
+ *
+ * @author : 박경서, 정유진, 김서현
+ * @since : 1.0
+ **/
+@Getter
+@AllArgsConstructor
+public class ModifyProductPolicyRequestDto {
+    @Length(max = 10)
+    private String policyMethod;
+    @NotNull
+    private boolean policySaved;
+    private Integer saveRate;
+}
