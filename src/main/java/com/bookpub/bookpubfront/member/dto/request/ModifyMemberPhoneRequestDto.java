15ad661722a0184018914ba92e6a40f986788200
HoFe-U <hochul-y@naver.com>
2023-01-20 01:12:18 +0900

    [Feat] : 회원 정보 수정 request Dto
    비밀번호 ,이름, 닉네임, 패스워드, 휴대전화
    
    Signed-off-by: HoFe-U <hochul-y@naver.com>

---
+++ src/main/java/com/bookpub/bookpubfront/member/dto/request/ModifyMemberPhoneRequestDto.java
@@ -0,0 +1,16 @@
+package com.bookpub.bookpubfront.member.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Some description here.
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
+@Getter
+@AllArgsConstructor
+public class ModifyMemberPhoneRequestDto {
+    private String phone;
+}
