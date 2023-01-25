28a4b60549d737182c94f9571d25fef5205fde29
limtaewon <tagkdj1@gmail.com>
2023-01-25 12:04:32 +0900

    [Refactor] provider 커스텀
    authentication manager가 실행시킬 authenticate() 메소드의 구현체

--- src/main/java/com/bookpub/bookpubfront/token/provider/CustomAuthenticationProvider.java
+++ src/main/java/com/bookpub/bookpubfront/token/provider/CustomAuthenticationProvider.java
@@ -1,11 +1,11 @@
 package com.bookpub.bookpubfront.token.provider;
 
-import com.bookpub.bookpubfront.token.CustomUserDetails;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 
 /**
  * 커스텀하여 만든 유저 인증 provider.
@@ -26,8 +26,8 @@ public class CustomAuthenticationProvider extends DaoAuthenticationProvider {
     public Authentication authenticate(
             Authentication authentication) throws AuthenticationException {
 
-        CustomUserDetails userDetails
-                = (CustomUserDetails) this.getUserDetailsService()
+        User userDetails
+                = (User) this.getUserDetailsService()
                 .loadUserByUsername((String) authentication.getPrincipal());
 
         return new UsernamePasswordAuthenticationToken(
