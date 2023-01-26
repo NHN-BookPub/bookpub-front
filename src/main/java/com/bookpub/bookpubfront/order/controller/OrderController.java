a0d3efdd4fe6bd6b0de58525ae8ec6c5e61ca34e
limtaewon <tagkdj1@gmail.com>
2023-01-26 21:27:16 +0900

    [Feat] 로직을 구성하고 adaptor를 실행시키는 service

--- src/main/java/com/bookpub/bookpubfront/order/controller/OrderController.java
+++ src/main/java/com/bookpub/bookpubfront/order/controller/OrderController.java
@@ -1,21 +1,31 @@
 package com.bookpub.bookpubfront.order.controller;
 
+import com.bookpub.bookpubfront.order.dto.response.GetAddressResponseDto;
+import com.bookpub.bookpubfront.order.service.OrderService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Some description here.
+ * 주문의 컨트롤러입니다.
  *
  * @author : 여운석, 임태원
  * @since : 1.0
  **/
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/order/test")
 public class OrderController {
+    private final OrderService orderService;
+
     @GetMapping
-    public String test() {
-        return"order/main";
+    public String test(Model model) {
+        List<GetAddressResponseDto> memberAddresses = orderService.getMemberAddresses();
+        model.addAttribute("address", memberAddresses);
+        return "order/main";
     }
 
 }
--- src/main/java/com/bookpub/bookpubfront/order/service/OrderService.java
+++ src/main/java/com/bookpub/bookpubfront/order/service/OrderService.java
@@ -1,10 +1,14 @@
 package com.bookpub.bookpubfront.order.service;
 
+import com.bookpub.bookpubfront.order.dto.response.GetAddressResponseDto;
+import java.util.List;
+
 /**
- * Some description here.
+ * 주문에 사용되는 서비스 인터페이스.
  *
- * @author : 여운석
+ * @author : 여운석, 임태원
  * @since : 1.0
  **/
 public interface OrderService {
+    List<GetAddressResponseDto> getMemberAddresses();
 }
