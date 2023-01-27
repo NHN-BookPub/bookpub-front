c4d6c34195571928e5ac4e8d74048646a895c4c4
limtaewon <tagkdj1@gmail.com>
2023-01-27 21:17:56 +0900

    [Feat] order 컨트롤러 작성

--- src/main/java/com/bookpub/bookpubfront/order/controller/MemberOrderController.java
+++ src/main/java/com/bookpub/bookpubfront/order/controller/MemberOrderController.java
@@ -1,8 +1,11 @@
 package com.bookpub.bookpubfront.order.controller;
 
+import com.bookpub.bookpubfront.member.dto.response.MemberDetailResponseDto;
+import com.bookpub.bookpubfront.member.service.MemberService;
 import com.bookpub.bookpubfront.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class MemberOrderController {
     private final OrderService orderService;
+    private final MemberService memberService;
 
     @GetMapping("/list")
     public String orderListView(Model model, Pageable pageable) {
@@ -28,10 +32,29 @@ public class MemberOrderController {
         return "mypage/orderList";
     }
 
-    @GetMapping()
+    @GetMapping("/detail")
     public String orderDetailView(Model model, @RequestParam Long orderNo) {
         model.addAttribute("orderDetail", orderService.getOrderDetailByNo(orderNo));
 
         return "mypage/orderDetail";
+    }
+
+    /**
+     * 회원, 비회원의 주문 view로 넘겨주는 컨트롤러.
+     *
+     * @param model view에 데이터 전달해주는 클래스.
+     * @return order view.
+     */
+    @GetMapping
+    public String memberOrder(Model model) {
+        String principal =
+                (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (!principal.equals("anonymousUser")) {
+            MemberDetailResponseDto member
+                    = memberService.getMember(Long.parseLong(principal));
+            model.addAttribute("member", member);
+        }
+        return "order/main";
     }
 }
