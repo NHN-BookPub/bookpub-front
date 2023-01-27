6a24abd14e5b646001b33e88811bad97a93e06bf
Woon <istana000@gmail.com>
2023-01-27 10:10:18 +0900

    [Feat] 에러 체크하는 util method 추가
    에러 체크하는 util method 추가

--- src/main/java/com/bookpub/bookpubfront/utils/Utils.java
+++ src/main/java/com/bookpub/bookpubfront/utils/Utils.java
@@ -7,7 +7,9 @@ import java.util.Objects;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -45,6 +47,28 @@ public class Utils {
                 .filter(cookie -> cookie.getName().equals(JwtUtil.JWT_SESSION))
                 .findAny()
                 .orElse(null);
+    }
+
+    /**
+     * 에러를 체크하기 위한 메서드입니다.
+     * 400, 500번대 에러를 거릅니다.
+     *
+     * @param response ResponseEntity 받습니다.
+     * @return 에러가 없으면 그대로 반환합니다.
+     * @param <T> 지네릭 타입입니다.
+     */
+    public static <T> ResponseEntity<T> checkError(ResponseEntity response) {
+        HttpStatus status = response.getStatusCode();
+
+        if (status.is4xxClientError()) {
+            throw new RuntimeException();
+        }
+
+        if (status.is5xxServerError()) {
+            throw new RuntimeException();
+        }
+
+        return response;
     }
 
 }
