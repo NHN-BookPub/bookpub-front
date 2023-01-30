b3d784d4821ddc85e45d61267cdfe4df737882fc
HoFe-U <hochul-y@naver.com>
2023-01-30 20:21:01 +0900

    [Refactor] : 피드백 반영
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/resources/static/js/tier/tierMain.js
+++ src/main/resources/static/js/tier/tierMain.js
@@ -8,7 +8,7 @@ function tierCheck() {
         url: "/tier-check",
         success: function (result) {
             if (result === true) {
-                alert("사용할수있는 아이디입니다.")
+                alert("사용할 수 있는 등급입니다.")
                 document.getElementById("tier-add-submit")
                     .disabled = false;
                 document.getElementById("")
