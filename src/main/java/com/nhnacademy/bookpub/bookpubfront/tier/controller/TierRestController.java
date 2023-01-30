a0294608fb5c5b55b2f7f2993f9506da29dd4a73
HoFe-U <hochul-y@naver.com>
2023-01-30 17:58:29 +0900

    [Feat] : 등급이름 Rest 통신 클래스
    Signed-off-by: HoFe-U <hochul-y@naver.com>

---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/tier/controller/TierRestController.java
@@ -0,0 +1,24 @@
+package com.nhnacademy.bookpub.bookpubfront.tier.controller;
+
+import com.nhnacademy.bookpub.bookpubfront.tier.service.TierService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * tier Rest 통신을 하기위한 컨트롤러 입니다.
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
+@RestController
+@RequiredArgsConstructor
+public class TierRestController {
+    private final TierService tierService;
+
+    @GetMapping("/tier-check")
+    public Boolean tierNameCheck(@RequestParam("tierName") String tierName){
+        return tierService.getTierName(tierName);
+    }
+}
