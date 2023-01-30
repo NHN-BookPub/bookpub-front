57d8ced66b0f6263fd4d54b881be964d8c538bd1
HoFe-U <hochul-y@naver.com>
2023-01-30 20:46:10 +0900

    [Refactor] :java doc 작성
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/tier/adaptor/TierAdaptor.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/tier/adaptor/TierAdaptor.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.bookpub.bookpubfront.tier.adaptor;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nhnacademy.bookpub.bookpubfront.tier.dto.request.CreateTierRequestDto;
 import com.nhnacademy.bookpub.bookpubfront.tier.dto.request.ModifyTierRequestDto;
 import com.nhnacademy.bookpub.bookpubfront.tier.dto.response.TierResponseDto;
@@ -14,13 +13,40 @@ import java.util.List;
  **/
 
 public interface TierAdaptor {
-    void requestAddTier(CreateTierRequestDto createRequestDto) throws JsonProcessingException;
+    /**
+     * 등급생성요청을하는 메서드입니다.
+     *
+     * @param createRequestDto 등급의 생성정보가 기입
+     */
+    void requestAddTier(CreateTierRequestDto createRequestDto);
 
-    void requestModifyTier(ModifyTierRequestDto modifyTierRequestDto) throws JsonProcessingException;
+    /**
+     * 등급의 정보를 수정하기위한 메서드입니다.
+     *
+     * @param modifyTierRequestDto 등급의 수정정보가 기입
+     */
+    void requestModifyTier(ModifyTierRequestDto modifyTierRequestDto);
 
+    /**
+     * 등급들의 정보를 받기위한 요청입니다.
+     *
+     * @return the list
+     */
     List<TierResponseDto> requestTierList();
 
+    /**
+     * 등급의 번호를 통해 등급을 불러오기위한 메서드입니다.
+     *
+     * @param tierNo 등급번호
+     * @return 등급정보가 반환됩니다.
+     */
     TierResponseDto requestTier(Integer tierNo);
 
+    /**
+     * 등급의 존재유무를 알려주는 메서드입니다.
+     *
+     * @param tierName 등급이름이 반환됩니다.
+     * @return boolean 값이 반환됩니다.
+     */
     Boolean requestTierName(String tierName);
 }
