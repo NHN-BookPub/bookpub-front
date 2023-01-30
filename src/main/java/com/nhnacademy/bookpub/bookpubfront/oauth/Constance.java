65f2ad556115da35a5e3a46b5e18969c30522f0f
limtaewon <tagkdj1@gmail.com>
2023-01-30 23:15:05 +0900

    [Feat] Constance 클래스
    많이쓰는 상수들 정리하여 사용

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/oauth/Constance.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/oauth/Constance.java
@@ -13,9 +13,14 @@ public class Constance {
 
     public static final String HTTPS = "https";
     public static final String GITHUB = "github.com";
+    public static final String KAKAO = "kauth.kakao.com";
+    public static final String NAVER = "nid.naver.com";
     public static final String CLIENT_ID = "client_id";
     public static final String CLIENT_SECRET = "client_secret";
-    public static final String REDIRECT_URL = "redirect_url";
+    public static final String REDIRECT_URI = "redirect_uri";
     public static final String BOOKPUB = "@bookpub.com";
+    public static final String GITHUB_EMAIL = "@github.com";
+    public static final String NAVER_EMAIL = "@naver.com";
+    public static final String KAKAO_EMAIL = "@kakao.com";
 
 }
