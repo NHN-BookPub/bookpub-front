30f1c3a5e353bd65ed8813324f4887e043352ef2
limtaewon <tagkdj1@gmail.com>
2023-01-30 19:11:50 +0900

    [Feat] 자주쓰는 값들 전역으로 설정

---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/oauth/Constance.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.bookpub.bookpubfront.oauth;
+
+/**
+ * oauth에 이용되는 상수들을 정리해둔 클래스.
+ *
+ * @author : 임태원
+ * @since : 1.0
+ **/
+public class Constance {
+
+    private Constance() {
+    }
+
+    public static final String HTTPS = "https";
+    public static final String GITHUB = "github.com";
+    public static final String CLIENT_ID = "client_id";
+    public static final String CLIENT_SECRET = "client_secret";
+    public static final String REDIRECT_URL = "redirect_url";
+    public static final String BOOKPUB = "@bookpub.com";
+
+}
