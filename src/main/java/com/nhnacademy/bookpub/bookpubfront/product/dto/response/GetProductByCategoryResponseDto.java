fbcf1947442b2c9a0cc6b9fc98276002a155fb43
Gyeongseo Bak <rudtj2966@gmail.com>
2023-02-01 01:46:24 +0900

    [Feat] 카테고리를 기준으로 상품정보를 담는 dto 구현

---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/product/dto/response/GetProductByCategoryResponseDto.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.bookpub.bookpubfront.product.dto.response;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+
+/**
+ * 카테고리를 기준으로 상품을 가져오는 dto.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+@Getter
+public class GetProductByCategoryResponseDto {
+    private Long productNo;
+    private String title;
+    private Long salesPrice;
+    private Integer salesRate;
+    private final List<String> categories = new ArrayList<>();
+    private final List<String> authors = new ArrayList<>();
+}
