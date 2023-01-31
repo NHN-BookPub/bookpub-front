36a567f663c6467eaa112f1c9b91cb09394925fa
Woon <istana000@gmail.com>
2023-01-31 14:26:52 +0900

    [Docs] 자바 독 추가
    자바 독 추가

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/pricepolicy/controller/PricePolicyController.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/pricepolicy/controller/PricePolicyController.java
@@ -25,6 +25,12 @@ public class PricePolicyController {
     private static final String PACKAGING = "packaging";
     private static final String POLICY = "policies";
 
+    /**
+     * 가격정책 메인페이지 뷰를 보여줍니다.
+     *
+     * @param model 모델.
+     * @return 가격정책 메인.
+     */
     @GetMapping
     public String pricePolicyView(Model model) {
         model.addAttribute(POLICY, pricePolicyService.getPricePolicies());
@@ -32,6 +38,13 @@ public class PricePolicyController {
         return "admin/pricepolicy/pricePolicyMain";
     }
 
+    /**
+     * policy 이름에 따른 리스트를 보여줍니다.
+     *
+     * @param model 모델.
+     * @param policyName 정책 이름.
+     * @return 정책 리스트 뷰
+     */
     @GetMapping("/{policyName}")
     public String individualPolicyView(Model model, @PathVariable String policyName) {
         if(policyName.equals(SHIPPING)) {
@@ -44,6 +57,12 @@ public class PricePolicyController {
         return "admin/pricepolicy/pricePolicyMain";
     }
 
+    /**
+     * 가격정책을 추가합니다.
+     *
+     * @param pricePolicyDto 추가를 위한 dto.
+     * @return 가격정책 메인페이지로 돌아갑니다.
+     */
     @PostMapping
     public String addPricePolicy(@ModelAttribute CreatePricePolicyRequestDto pricePolicyDto) {
         log.info("dto = {}", pricePolicyDto);
@@ -52,6 +71,11 @@ public class PricePolicyController {
         return "redirect:/admin/pricepolicies";
     }
 
+    /**
+     * 가격정책을 더해주는 뷰로 이동합니다.
+     *
+     * @return 추가등록 뷰.
+     */
     @GetMapping("/add")
     public String addPricePolicyView() {
         return "admin/pricepolicy/pricePolicyAdd";
