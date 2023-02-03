9a3d20405780e6d9941a40ae8634c34ad660c690
seohyeon <kgustjgus@naver.com>
2023-02-03 09:24:41 +0900

    [Feat] : 등급 쿠폰 service

---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/tiercoupon/service/TierCouponService.java
@@ -0,0 +1,38 @@
+package com.nhnacademy.bookpub.bookpubfront.tiercoupon.service;
+
+import com.nhnacademy.bookpub.bookpubfront.tiercoupon.dto.request.CreateTierCouponRequestDto;
+import com.nhnacademy.bookpub.bookpubfront.tiercoupon.dto.response.GetTierCouponResponseDto;
+import com.nhnacademy.bookpub.bookpubfront.utils.PageResponse;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * 등급 쿠폰을 다루기 위한 서비스 인터페이스입니다.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+public interface TierCouponService {
+
+    /**
+     * 등급 쿠폰 페이지를 조회하기 위한 메서드 입니다.
+     *
+     * @param pageable 페이지.
+     * @return 등급 쿠폰 정보 DTO를 담은 페이지.
+     */
+    PageResponse<GetTierCouponResponseDto> getTierCoupons(Pageable pageable);
+
+    /**
+     * 등급 쿠폰 생성하기 위한 메서드입니다.
+     *
+     * @param createRequestDto 쿠폰 생성에 필요한 정보를 담은 DTO.
+     */
+    void createTierCoupon(CreateTierCouponRequestDto createRequestDto);
+
+    /**
+     * 해당 등급 쿠폰을 삭제하기 위한 메서드입니다.
+     *
+     * @param tierNo     등급 번호.
+     * @param templateNo 쿠폰 템플릿 번호.
+     */
+    void deleteTierCoupon(Integer tierNo, Long templateNo);
+}
---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/tiercoupon/service/impl/TierCouponServiceImpl.java
@@ -0,0 +1,49 @@
+package com.nhnacademy.bookpub.bookpubfront.tiercoupon.service.impl;
+
+import com.nhnacademy.bookpub.bookpubfront.tiercoupon.adaptor.TierCouponAdaptor;
+import com.nhnacademy.bookpub.bookpubfront.tiercoupon.dto.request.CreateTierCouponRequestDto;
+import com.nhnacademy.bookpub.bookpubfront.tiercoupon.dto.response.GetTierCouponResponseDto;
+import com.nhnacademy.bookpub.bookpubfront.tiercoupon.service.TierCouponService;
+import com.nhnacademy.bookpub.bookpubfront.utils.PageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * 등급 쿠폰을 다루기 위한 구현체입니다.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+@Service
+@RequiredArgsConstructor
+public class TierCouponServiceImpl implements TierCouponService {
+
+    private final TierCouponAdaptor tierCouponAdaptor;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public PageResponse<GetTierCouponResponseDto> getTierCoupons(Pageable pageable) {
+        return tierCouponAdaptor.requestTierCoupons(pageable);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void createTierCoupon(CreateTierCouponRequestDto createRequestDto) {
+        tierCouponAdaptor.requestAddTierCoupon(createRequestDto);
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void deleteTierCoupon(Integer tierNo, Long templateNo) {
+        tierCouponAdaptor.requestDeleteTierCoupon(tierNo, templateNo);
+
+    }
+}
