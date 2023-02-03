19fc7f9654a35d2dcbca054037b87821415cdcf9
limtaewon <tagkdj1@gmail.com>
2023-02-03 13:50:55 +0900

    [Feat] 상품 view를 구성하는 요소

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/order/controller/MemberOrderController.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/order/controller/MemberOrderController.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -30,9 +31,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/orders")
 @RequiredArgsConstructor
 public class MemberOrderController {
+    private static final String MEMBER = "member";
     private final OrderService orderService;
     private final MemberService memberService;
     private final ProductService productService;
+
 
     /**
      * 멤버의 주문 리스트 조회를 위한 메소드입니다.
@@ -50,7 +53,7 @@ public class MemberOrderController {
                         .getAuthentication()
                         .getPrincipal());
 
-        model.addAttribute("member", memberService.getMember(memberNo));
+        model.addAttribute(MEMBER, memberService.getMember(memberNo));
         model.addAttribute("orderList", orderService.getOrderListByMemberNo(memberNo, pageable));
         model.addAttribute("nowPage", pageable.getPageNumber());
 
@@ -72,7 +75,7 @@ public class MemberOrderController {
                         .getAuthentication()
                         .getPrincipal());
 
-        model.addAttribute("member", memberService.getMember(memberNo));
+        model.addAttribute(MEMBER, memberService.getMember(memberNo));
         model.addAttribute("orderDetail", orderService.getOrderDetailByNo(orderNo));
 
         return "mypage/orderDetail";
@@ -87,22 +90,30 @@ public class MemberOrderController {
     @GetMapping("/order")
     public String memberOrder(Model model,
                               @CookieValue(name = "orderInfo", required = false) String cart) {
+        boolean isLoginUser = false;
         String principal =
                 (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         if (!principal.equals("anonymousUser")) {
             MemberDetailResponseDto member
                     = memberService.getMember(Long.parseLong(principal));
-            model.addAttribute("member", member);
+            isLoginUser = true;
+            model.addAttribute(MEMBER, member);
         }
 
         if (Objects.nonNull(cart)) {
             List<String> products = CartUtils.parsingCart(cart);
-            List<OrderProductDto> orderProductDtoList = productService.orderProductInCart(products);
+            List<OrderProductDto> orderProductDtoList =
+                    productService.orderProductInCart(products, isLoginUser);
 
             model.addAttribute("products", orderProductDtoList);
         }
 
         return "order/main";
+    }
+
+    @PostMapping("/order")
+    public String orderComplete() {
+        return "redirect:/";
     }
 }
