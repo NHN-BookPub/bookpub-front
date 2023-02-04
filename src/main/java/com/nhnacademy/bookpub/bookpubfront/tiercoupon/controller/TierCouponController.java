ec579f00eb922a25f8afe170541c06db2f393f25
seohyeon <kgustjgus@naver.com>
2023-02-04 20:09:46 +0900

    [Feat] : 등급쿠폰 발급 페이지로 이동

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/tiercoupon/controller/TierCouponController.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/tiercoupon/controller/TierCouponController.java
@@ -1,18 +1,24 @@
 package com.nhnacademy.bookpub.bookpubfront.tiercoupon.controller;
 
+import com.nhnacademy.bookpub.bookpubfront.cart.util.CartUtils;
+import com.nhnacademy.bookpub.bookpubfront.category.util.CategoryUtils;
+import com.nhnacademy.bookpub.bookpubfront.member.dto.response.MemberDetailResponseDto;
+import com.nhnacademy.bookpub.bookpubfront.member.service.MemberService;
 import com.nhnacademy.bookpub.bookpubfront.tiercoupon.dto.request.CreateTierCouponRequestDto;
 import com.nhnacademy.bookpub.bookpubfront.tiercoupon.dto.response.GetTierCouponResponseDto;
 import com.nhnacademy.bookpub.bookpubfront.tiercoupon.service.TierCouponService;
 import com.nhnacademy.bookpub.bookpubfront.utils.PageResponse;
 import java.util.List;
+import javax.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -22,11 +28,17 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @since : 1.0
  **/
 @RequiredArgsConstructor
-@RequestMapping("/admin/coupon/tier-coupons")
 @Controller
 public class TierCouponController {
 
     private final TierCouponService tierCouponService;
+    private final MemberService memberService;
+
+    private final CartUtils cartUtils;
+
+    private final CategoryUtils categoryUtils;
+
+    private static final String CART = "CART";
 
     /**
      * 등급 쿠폰 목록 조회 메서드입니다.
@@ -35,7 +47,7 @@ public class TierCouponController {
      * @param pageable 페이지 정보
      * @return 등급 쿠폰 관리자 페이지
      */
-    @GetMapping
+    @GetMapping("/admin/coupon/tier-coupons")
     public String tierCouponList(Model model, @PageableDefault Pageable pageable) {
         PageResponse<GetTierCouponResponseDto> tierCoupons = tierCouponService.getTierCoupons(
                 pageable);
@@ -57,7 +69,7 @@ public class TierCouponController {
      * @param createRequestDto 등급 쿠폰 등록을 위한 정보
      * @return 관리자 등급 쿠폰 페이지
      */
-    @PostMapping
+    @PostMapping("/admin/coupon/tier-coupons")
     public String addTierCoupon(CreateTierCouponRequestDto createRequestDto) {
         tierCouponService.createTierCoupon(createRequestDto);
 
@@ -72,11 +84,39 @@ public class TierCouponController {
      * @param templateNo 쿠폰 템플릿 번호
      * @return 관리자 등급 쿠폰 페이지
      */
-    @PostMapping("/delete")
+    @PostMapping("/admin/coupon/tier-coupons/delete")
     public String deleteTierCoupon(@RequestParam Integer tierNo, @RequestParam Long templateNo) {
         tierCouponService.deleteTierCoupon(tierNo, templateNo);
 
         return "redirect:/admin/coupon/tier-coupons";
+    }
+
+    /**
+     * 등급 쿠폰 발급 페이지로 이동합니다.
+     *
+     * @return 등급 쿠폰 발급 페이지
+     */
+    @GetMapping("/tier-coupons")
+    public String tierCouponEvent(@CookieValue(name = CART, required = false) Cookie cookie,
+            Model model) {
+        SecurityContextHolder.getContext().getAuthentication();
+
+        String principal =
+                (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long memberNo = -1L;
+
+        if (!principal.equals("anonymousUser")) {
+            MemberDetailResponseDto member
+                    = memberService.getMember(Long.parseLong(principal));
+            memberNo = member.getMemberNo();
+        }
+
+        cartUtils.getCountInCart(cookie.getValue(), model);
+        categoryUtils.categoriesView(model);
+
+        model.addAttribute("member", memberNo);
+
+        return "coupon/tierCoupon";
     }
 
 }
