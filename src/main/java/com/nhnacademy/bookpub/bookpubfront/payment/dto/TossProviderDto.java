744f9cb5ef03410402cc0fabe0e1fbae1b1e4d64
limtaewon <tagkdj1@gmail.com>
2023-02-05 16:40:29 +0900

    [Feat] js에서 이용할 toss dto

---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/payment/dto/TossProviderDto.java
@@ -0,0 +1,19 @@
+package com.nhnacademy.bookpub.bookpubfront.payment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 토스 결제서비스 사용시 필요한 정보를 모아놓은 dto.
+ *
+ * @author : 임태원
+ * @since : 1.0
+ **/
+@AllArgsConstructor
+@Getter
+public class TossProviderDto {
+    private String clientId;
+    private String secret;
+    private String successUrl;
+    private String failUrl;
+}
