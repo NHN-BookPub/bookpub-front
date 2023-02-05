60879c6cd860e4fabf461678d510b4545baf0254
Woon <istana000@gmail.com>
2023-02-05 22:35:45 +0900

    [Feat] 응답에 따라 발생되는 예외 추가
    응답에 따라 발생되는 예외 추가

---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/exception/BadGatewayException.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.bookpub.bookpubfront.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * 502발생시 예외입니다.
+ *
+ * @author : 여운석
+ * @since : 1.0
+ **/
+@ResponseStatus(HttpStatus.BAD_GATEWAY)
+public class BadGatewayException extends RuntimeException {
+    public BadGatewayException() {
+        super();
+    }
+}
---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/exception/BadRequestException.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.bookpub.bookpubfront.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * 400발생시 예외입니다.
+ *
+ * @author : 여운석
+ * @since : 1.0
+ **/
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+    private static final String MESSAGE = "잘못된 요청입니다.";
+    public BadRequestException() {
+        super(MESSAGE);
+    }
+}
