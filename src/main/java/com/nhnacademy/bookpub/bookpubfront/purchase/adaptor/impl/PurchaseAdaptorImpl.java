95df9fd8d8602d9273582648e4e44fb5d0734124
Woon <istana000@gmail.com>
2023-02-08 18:30:43 +0900

    [Feat] 매입이력 어답터 추가
    매입이력 어답터 추가

---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/purchase/adaptor/impl/PurchaseAdaptorImpl.java
@@ -0,0 +1,93 @@
+package com.nhnacademy.bookpub.bookpubfront.purchase.adaptor.impl;
+
+import static com.nhnacademy.bookpub.bookpubfront.utils.Utils.makeHeader;
+import com.nhnacademy.bookpub.bookpubfront.config.GateWayConfig;
+import com.nhnacademy.bookpub.bookpubfront.purchase.adaptor.PurchaseAdaptor;
+import com.nhnacademy.bookpub.bookpubfront.purchase.dto.request.CreatePurchaseRequestDto;
+import com.nhnacademy.bookpub.bookpubfront.purchase.dto.response.GetPurchaseListResponseDto;
+import com.nhnacademy.bookpub.bookpubfront.utils.PageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * 구매이력 아답터의 구현체입니다.
+ *
+ * @author : 여운석
+ * @since : 1.0
+ **/
+@Component
+@RequiredArgsConstructor
+public class PurchaseAdaptorImpl implements PurchaseAdaptor {
+    private final RestTemplate restTemplate;
+    private static final String AUTH_PURCHASE_URL = "/token/purchases";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public PageResponse<GetPurchaseListResponseDto> getPurchases(Pageable pageable) {
+        String url =
+                UriComponentsBuilder.fromHttpUrl(GateWayConfig.getGatewayUrl() + AUTH_PURCHASE_URL)
+                        .queryParam("page", pageable.getPageNumber())
+                        .queryParam("size", pageable.getPageSize())
+                        .encode()
+                        .toUriString();
+
+        ResponseEntity<PageResponse<GetPurchaseListResponseDto>> response =
+                restTemplate.exchange(url,
+                        HttpMethod.GET,
+                        new HttpEntity<>(makeHeader()),
+                        new ParameterizedTypeReference<>() {
+                        });
+
+        return response.getBody();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void createPurchase(CreatePurchaseRequestDto request) {
+        String url = UriComponentsBuilder.fromHttpUrl(
+                GateWayConfig.getGatewayUrl() + AUTH_PURCHASE_URL + "/absorption")
+                .encode()
+                .toUriString();
+
+        HttpEntity<CreatePurchaseRequestDto> entity =
+                new HttpEntity<>(request, makeHeader());
+
+        restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                entity,
+                Void.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public PageResponse<GetPurchaseListResponseDto> getPurchasesByProductNo(Long productNo, Pageable pageable) {
+        String url = UriComponentsBuilder.fromHttpUrl(
+                GateWayConfig.getGatewayUrl() + AUTH_PURCHASE_URL + "/" + productNo)
+                .encode()
+                .toUriString();
+
+        ResponseEntity<PageResponse<GetPurchaseListResponseDto>> response =
+                restTemplate.exchange(
+                        url,
+                        HttpMethod.GET,
+                        new HttpEntity<>(makeHeader()),
+                        new ParameterizedTypeReference<>() {});
+
+        return response.getBody();
+    }
+
+}
