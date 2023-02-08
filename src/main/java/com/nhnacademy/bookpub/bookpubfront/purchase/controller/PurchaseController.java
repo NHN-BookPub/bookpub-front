0b6b1d1ef4de6b5d27c929e6d3ecc374e1003154
Woon <istana000@gmail.com>
2023-02-08 22:24:09 +0900

    [Docs] 매입이력 컨트롤러 javaDoc 추가
    매입이력 컨트롤러 javaDoc 추가

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/purchase/controller/PurchaseController.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/purchase/controller/PurchaseController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * 구매이력 컨트롤러 입니다.
+ * 모든 메소드는 관리자만 이용할 수 있습니다.
  *
  * @author : 여운석
  * @since : 1.0
@@ -27,6 +28,13 @@ public class PurchaseController {
     private final PurchaseService purchaseService;
     private static final String REDIRECT = "redirect:/admin/purchases";
 
+    /**
+     * 최근 등록순으로 모든 매입이력을 조회합니다.
+     *
+     * @param model 모델
+     * @param pageable 페이징
+     * @return 모든 매입이력(최신순)
+     */
     @Auth
     @GetMapping("/admin/purchases")
     public String getPurchases(Model model, @PageableDefault Pageable pageable) {
@@ -41,6 +49,13 @@ public class PurchaseController {
         return "/admin/product/purchaseIndex";
     }
 
+    /**
+     * 매입이력을 등록합니다.
+     * 상품의 재고도 함께 증가합니다.
+     *
+     * @param requestDto 등록시 필요한 dto
+     * @return 매입이력 조회로 다시 이동
+     */
     @Auth
     @PostMapping("/admin/purchases")
     public String createPurchase(CreatePurchaseRequestDto requestDto) {
@@ -49,6 +64,14 @@ public class PurchaseController {
         return REDIRECT;
     }
 
+    /**
+     * 상품번호로 이력을 조회합니다.
+     *
+     * @param model 모델
+     * @param pageable 페이징
+     * @param productNo 상품번호
+     * @return 상품 단건에 대한 매입이력
+     */
     @Auth
     @GetMapping("/admin/purchases/{productNo}")
     public String getPurchasesByProductNo(Model model,
