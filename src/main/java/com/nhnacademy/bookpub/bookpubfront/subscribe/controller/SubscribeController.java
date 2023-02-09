c1c5c6e887c6dca0d464e5e6011993493630ac6b
HoFe-U <hochul-y@naver.com>
2023-02-09 16:10:21 +0900

    [Feat] : 구독연관상품, 삭제,갱신 관련 추가
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/subscribe/controller/SubscribeController.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/subscribe/controller/SubscribeController.java
@@ -2,17 +2,22 @@ package com.nhnacademy.bookpub.bookpubfront.subscribe.controller;
 
 import com.nhnacademy.bookpub.bookpubfront.annotation.Auth;
 import com.nhnacademy.bookpub.bookpubfront.subscribe.dto.request.CreateSubscribeRequestDto;
+import com.nhnacademy.bookpub.bookpubfront.subscribe.dto.request.ModifySubscribeRequestDto;
+import com.nhnacademy.bookpub.bookpubfront.subscribe.dto.response.GetSubscribeDetailResponseDto;
 import com.nhnacademy.bookpub.bookpubfront.subscribe.dto.response.GetSubscribeResponseDto;
 import com.nhnacademy.bookpub.bookpubfront.subscribe.service.SubscribeService;
 import com.nhnacademy.bookpub.bookpubfront.utils.PageResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,10 +27,13 @@ import org.springframework.web.multipart.MultipartFile;
  * @author : 유호철
  * @since : 1.0
  **/
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class SubscribeController {
     private final SubscribeService service;
+
+    private static final String RE_SUBSCRIBE = "redirect:/admin/subscribes";
 
     @GetMapping("/admin/subscribes")
     public String subscribeMain(@PageableDefault Pageable pageable,
@@ -42,6 +50,7 @@ public class SubscribeController {
         model.addAttribute("nextPageNo", subscribes.getNumber() + 1);
         model.addAttribute("size", pageable.getPageSize());
         model.addAttribute("uri", "/admin/subscribes");
+
         return "admin/subscribe/subscribeList";
     }
 
@@ -50,6 +59,63 @@ public class SubscribeController {
     public String subscribeAdd(@ModelAttribute CreateSubscribeRequestDto dto,
                                @RequestPart MultipartFile image) {
         service.addSubscribe(dto, image);
-        return "redirect:/admin/subscribes";
+
+        return RE_SUBSCRIBE;
     }
+
+    @Auth
+    @PostMapping("/admin/subscribes/{subscribeNo}")
+    public String subscribeModify(@ModelAttribute ModifySubscribeRequestDto dto,
+                                  @RequestPart MultipartFile image,
+                                  @PathVariable("subscribeNo") Long subscribeNo) {
+        service.modifySubscribe(subscribeNo, image, dto);
+        return "redirect:/admin/subscribe/" + subscribeNo;
+    }
+
+
+    @GetMapping("/admin/subscribes/{subscribeNo}")
+    public String subscribeDetails(@PathVariable("subscribeNo") Long subscribeNo,
+                                   Model model) {
+        GetSubscribeDetailResponseDto subscribe = service.getSubscribeDetail(subscribeNo);
+        model.addAttribute("subscribe", subscribe);
+        model.addAttribute("content", subscribe.getProductLists());
+
+        return "/admin/subscribe/subscribesDetail";
+    }
+
+    @Auth
+    @PostMapping("/admin/subscribes-removed/{subscribeNo}")
+    public String subscribeDelete(@PathVariable("subscribeNo") Long subscribeNo,
+                                  @RequestParam("deleted") boolean deleted,
+                                  Model model) {
+        service.deleteSubscribe(subscribeNo, deleted);
+        return RE_SUBSCRIBE;
+    }
+
+    @Auth
+    @PostMapping("/admin/subscribes-renewed/{subscribeNo}")
+    public String subscribeRenewed(@PathVariable("subscribeNo") Long subscribeNo,
+                                   @RequestParam("renewed") boolean renewed) {
+        service.renewedSubscribe(subscribeNo, renewed);
+        return RE_SUBSCRIBE;
+    }
+
+    @Auth
+    @PostMapping("/admin/subscribes-product-list/{subscribeNo}")
+    public String subscribeProductList(@PathVariable("subscribeNo") Long subscribeNo,
+                                       @RequestParam("products") String products) {
+        log.error(products);
+        service.addSubscribeProductList(subscribeNo, products);
+        return "redirect:/admin/subscribes/" + subscribeNo;
+    }
+
+    @GetMapping("/subscribes")
+    public String mainSubscribes(@PageableDefault Pageable pageable,
+                                 Model model){
+        PageResponse<GetSubscribeResponseDto> subscribes = service.getSubscribeList(pageable);
+        model.addAttribute("subscribes", subscribes);
+
+        return null;
+    }
+
 }
--- src/main/java/com/nhnacademy/bookpub/bookpubfront/subscribe/service/SubscribeService.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/subscribe/service/SubscribeService.java
@@ -2,6 +2,7 @@ package com.nhnacademy.bookpub.bookpubfront.subscribe.service;
 
 import com.nhnacademy.bookpub.bookpubfront.subscribe.dto.request.CreateSubscribeRequestDto;
 import com.nhnacademy.bookpub.bookpubfront.subscribe.dto.request.ModifySubscribeRequestDto;
+import com.nhnacademy.bookpub.bookpubfront.subscribe.dto.response.GetSubscribeDetailResponseDto;
 import com.nhnacademy.bookpub.bookpubfront.subscribe.dto.response.GetSubscribeResponseDto;
 import com.nhnacademy.bookpub.bookpubfront.utils.PageResponse;
 import org.springframework.data.domain.Pageable;
@@ -27,7 +28,7 @@ public interface SubscribeService {
      * @param scribeNo 구독번호
      * @param dto      수정할 구독 정보
      */
-    void modifySubscribe(Long scribeNo, ModifySubscribeRequestDto dto);
+    void modifySubscribe(Long scribeNo, MultipartFile image, ModifySubscribeRequestDto dto);
 
     /**
      * 구독들의 정보들이 반환됩니다.
@@ -36,4 +37,37 @@ public interface SubscribeService {
      * @return 페이징된 정보가 반환됩니다.
      */
     PageResponse<GetSubscribeResponseDto> getSubscribeList(Pageable pageable);
+
+    /**
+     * 구독상품의 상세정보가 반환됩니다.
+     *
+     * @param subscribeNo 구독번호.
+     * @return 구독상세정보가 반환됩니다.
+     */
+    GetSubscribeDetailResponseDto getSubscribeDetail(Long subscribeNo);
+
+    /**
+     * 구독 정보 삭제상태로 변경
+     *
+     * @param subscribeNo the subscribe no
+     * @param deleted     the deleted
+     */
+    void deleteSubscribe(Long subscribeNo, boolean deleted);
+
+
+    /**
+     * 갱신 여부 변경
+     *
+     * @param subscribeNo 구독번호 기입
+     * @param renewed     갱신여부
+     */
+    void renewedSubscribe(Long subscribeNo, boolean renewed);
+
+    /**
+     * 구독에 구독상품을 추가하기위한 메서드입니다.
+     *
+     * @param subscribeNo 구독번호
+     * @param products    상품번호들.
+     */
+    void addSubscribeProductList(Long subscribeNo, String products);
 }
