8defb0915239b1a835c7312b2049af8753521bfe
Gyeongseo Bak <rudtj2966@gmail.com>
2023-02-10 11:35:06 +0900

    [Feat] File 관련 서비스 구현

---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/file/service/FileService.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.bookpub.bookpubfront.file.service;
+
+import com.nhnacademy.bookpub.bookpubfront.file.dto.response.GetDownloadInfo;
+
+/**
+ * File 관련 서비스 인터페이스.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+public interface FileService {
+
+    /**
+     * 쿠폰 템플릿 정보 호출 메서드.
+     *
+     * @param templateNo 템플릿 번호
+     * @return 템플릿 정보
+     */
+    GetDownloadInfo downloadCouponTemplateInfo(Long templateNo);
+
+    /**
+     * E-Book 정보 호출 메서드.
+     *
+     * @param productNo 상품 번호
+     * @return 상품 정보
+     */
+    GetDownloadInfo downloadEBookInfo(Long productNo);
+
+    /**
+     * File Download 메서드.
+     *
+     * @param path  파일이 저장되어있는 경로
+     * @param token 오브젝트 스토리지 토큰
+     * @return byte 단위 파일
+     */
+    byte[] downloadFile(String path, String token);
+}
---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/file/service/impl/FileServiceImpl.java
@@ -0,0 +1,62 @@
+package com.nhnacademy.bookpub.bookpubfront.file.service.impl;
+
+import com.nhnacademy.bookpub.bookpubfront.file.adaptor.FileAdaptor;
+import com.nhnacademy.bookpub.bookpubfront.file.dto.response.GetDownloadInfo;
+import com.nhnacademy.bookpub.bookpubfront.file.service.FileService;
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * FileService 구현체.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+@Service
+@RequiredArgsConstructor
+public class FileServiceImpl implements FileService {
+    private final FileAdaptor fileAdaptor;
+    private final RestTemplate restTemplate;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GetDownloadInfo downloadCouponTemplateInfo(Long templateNo) {
+        return fileAdaptor.requestCouponTemplateInfo(templateNo);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GetDownloadInfo downloadEBookInfo(Long productNo) {
+        return fileAdaptor.requestEBookInfo(productNo);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public byte[] downloadFile(String path, String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Auth-Token", token);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+
+        ResponseEntity<byte[]> response = restTemplate.exchange(
+                path,
+                HttpMethod.GET,
+                new HttpEntity<String>(null, headers),
+                byte[].class
+        );
+
+        return response.getBody();
+    }
+}
