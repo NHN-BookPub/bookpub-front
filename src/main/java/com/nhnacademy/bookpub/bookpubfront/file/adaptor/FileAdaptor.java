9d0abb5e1102918b34211e2da552a3b9ce720ae4
Gyeongseo Bak <rudtj2966@gmail.com>
2023-02-10 11:35:25 +0900

    [Feat] File 관련 어댑터 구현

---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/file/adaptor/FileAdaptor.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.bookpub.bookpubfront.file.adaptor;
+
+import com.nhnacademy.bookpub.bookpubfront.file.dto.response.GetDownloadInfo;
+
+/**
+ * File API 부르기 위한 어댑터.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+public interface FileAdaptor {
+
+    /**
+     * 쿠폰 템플릿 정보 조회 API 호출.
+     *
+     * @param templateNo 템플릿 번호
+     * @return 템플릿 오브젝트 스토리지 정보
+     */
+    GetDownloadInfo requestCouponTemplateInfo(Long templateNo);
+
+    /**
+     * E-Book 정보 조회 API 호출.
+     *
+     * @param productNo 싱픔 반호
+     * @return E-Book 오브젝트 스토리지 정보
+     */
+    GetDownloadInfo requestEBookInfo(Long productNo);
+}
