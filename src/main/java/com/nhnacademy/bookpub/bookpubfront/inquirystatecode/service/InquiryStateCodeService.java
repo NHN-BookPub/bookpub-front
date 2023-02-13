d943172ddc0f17ddaa5462bc037d70cda3f9719c
hobby3530 <jungy3530@gmail.com>
2023-02-13 11:12:17 +0900

    [Refactor] 상품문의상태코드 관련 수정

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/inquirystatecode/service/InquiryStateCodeService.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/inquirystatecode/service/InquiryStateCodeService.java
@@ -4,12 +4,17 @@ import com.nhnacademy.bookpub.bookpubfront.inquirystatecode.dto.response.GetInqu
 import java.util.List;
 
 /**
- * Some description here.
+ * 상품문의상태코드 서비스 인터페이스입니다.
  *
  * @author : 정유진
  * @since : 1.0
  **/
 public interface InquiryStateCodeService {
 
+    /**
+     * 회원들이 사용할 수 있는 상품문의상태코드 리스트 조회를 위한 메서드입니다.
+     *
+     * @return 상품문의상태코드 정보를 담은 dto 리스트
+     */
     List<GetInquiryStateCodeResponseDto> getInquiryStateCodeListForMember();
 }
