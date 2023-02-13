55dd71f1ee32f32a9cbc9558382dcdfcaf096489
Woon <istana000@gmail.com>
2023-02-13 15:37:07 +0900

    [Feat] 고객서비스 아답터 추가
    고객서비스 아답터 추가

---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/customerservice/adaptor/CustomerServiceAdaptor.java
@@ -0,0 +1,47 @@
+package com.nhnacademy.bookpub.bookpubfront.customerservice.adaptor;
+
+import com.nhnacademy.bookpub.bookpubfront.customerservice.dto.GetCustomerServiceListResponseDto;
+import com.nhnacademy.bookpub.bookpubfront.utils.PageResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.util.MultiValueMap;
+
+/**
+ * 고객서비스의 아답터.
+ *
+ * @author : 여운석
+ * @since : 1.0
+ **/
+public interface CustomerServiceAdaptor {
+
+    /**
+     * 고객서비스를 생성합니다.
+     *
+     * @param requestMap 생성에 필요한 정보들
+     */
+    void createCustomerService(MultiValueMap<String, Object> requestMap);
+
+    /**
+     * 모든 고객서비스를 조회합니다.
+     * @param pageable 페이징
+     * @return 고객서비스 리스트
+     */
+    PageResponse<GetCustomerServiceListResponseDto> getCustomerServices(Pageable pageable);
+
+    /**
+     * 코드명으로 고객서비스를 조회합니다.
+     *
+     * @param name 코드명
+     * @param pageable 페이징
+     * @return 고객서비스 리스트
+     */
+    PageResponse<GetCustomerServiceListResponseDto> getCustomerServiceByCodeName(String name, Pageable pageable);
+
+    /**
+     *
+     * 카테고리로 고객서비스를 조회합니다.
+     * @param category 카테고리
+     * @param pageable 페이징
+     * @return 고객서비스 리스트
+     */
+    PageResponse<GetCustomerServiceListResponseDto> getCustomerServiceByCategory(String category, Pageable pageable);
+}
---
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/customerservice/adaptor/impl/CustomerServiceAdaptorImpl.java
@@ -0,0 +1,119 @@
+package com.nhnacademy.bookpub.bookpubfront.customerservice.adaptor.impl;
+
+import static com.nhnacademy.bookpub.bookpubfront.utils.Utils.makeHeader;
+import com.nhnacademy.bookpub.bookpubfront.config.GateWayConfig;
+import com.nhnacademy.bookpub.bookpubfront.customerservice.adaptor.CustomerServiceAdaptor;
+import com.nhnacademy.bookpub.bookpubfront.customerservice.dto.GetCustomerServiceListResponseDto;
+import com.nhnacademy.bookpub.bookpubfront.utils.PageResponse;
+import com.nhnacademy.bookpub.bookpubfront.utils.Utils;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * 고객서비스 아답터의 구현체.
+ *
+ * @author : 여운석
+ * @since : 1.0
+ **/
+@Component
+@RequiredArgsConstructor
+public class CustomerServiceAdaptorImpl implements CustomerServiceAdaptor {
+    private final RestTemplate restTemplate;
+    private static final String AUTH_SERVICE_URL = "/token/services";
+    private static final String SERVICE_URL = "/api/services";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void createCustomerService(MultiValueMap<String, Object> requestMap) {
+        String url = GateWayConfig.getGatewayUrl() + AUTH_SERVICE_URL;
+
+        HttpEntity<MultiValueMap<String, Object>> entity =
+                new HttpEntity<>(requestMap,
+                        makeHeader(MediaType.MULTIPART_FORM_DATA,
+                                List.of(MediaType.MULTIPART_FORM_DATA, MediaType.APPLICATION_JSON)));
+
+        restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                entity,
+                Void.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public PageResponse<GetCustomerServiceListResponseDto> getCustomerServices(Pageable pageable) {
+        String url = UriComponentsBuilder.fromHttpUrl(GateWayConfig.getGatewayUrl() + AUTH_SERVICE_URL)
+                .queryParam("page", pageable.getPageNumber())
+                .queryParam("size", pageable.getPageSize())
+                .encode()
+                .toUriString();
+
+        return restTemplate.exchange(
+                        url,
+                        HttpMethod.GET,
+                        new HttpEntity<>(Utils.makeHeader()),
+                        new ParameterizedTypeReference<PageResponse<GetCustomerServiceListResponseDto>>() {
+                        })
+                .getBody();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public PageResponse<GetCustomerServiceListResponseDto>
+    getCustomerServiceByCodeName(String name, Pageable pageable) {
+        String url = UriComponentsBuilder
+                .fromHttpUrl(GateWayConfig.getGatewayUrl()
+                        + SERVICE_URL
+                        + "/" + name)
+                .queryParam("page", pageable.getPageNumber())
+                .queryParam("size", pageable.getPageSize())
+                .encode()
+                .toUriString();
+
+        return restTemplate.exchange(
+                        url,
+                        HttpMethod.GET,
+                        new HttpEntity<>(Utils.makeHeader()),
+                        new ParameterizedTypeReference<PageResponse<GetCustomerServiceListResponseDto>>() {
+                        })
+                .getBody();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public PageResponse<GetCustomerServiceListResponseDto> getCustomerServiceByCategory(String category, Pageable pageable) {
+        String url = UriComponentsBuilder
+                .fromHttpUrl(GateWayConfig.getGatewayUrl()
+                        + SERVICE_URL
+                        + "/category/" + category)
+                .queryParam("page", pageable.getPageNumber())
+                .queryParam("size", pageable.getPageSize())
+                .encode()
+                .toUriString();
+
+        return restTemplate.exchange(
+                        url,
+                        HttpMethod.GET,
+                        new HttpEntity<>(Utils.makeHeader()),
+                        new ParameterizedTypeReference<PageResponse<GetCustomerServiceListResponseDto>>() {
+                        })
+                .getBody();
+    }
+}
