656744091333739560bdbbb9383fc80ae50954d8
hobby3530 <jungy3530@gmail.com>
2023-02-13 11:17:34 +0900

    [Refactor] 상품문의 관련 컨트롤러 수정

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/inquiry/controller/AdminInquiryController.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/inquiry/controller/AdminInquiryController.java
@@ -16,9 +16,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * 상품문의를 다루기 위한 컨트롤러입니다.
+ * 상품문의 관리자 페이지를 다루기 위한 컨트롤러입니다.
  *
  * @author : 정유진
  * @since : 1.0
@@ -28,18 +29,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AdminInquiryController {
     private final InquiryService inquiryService;
 
+    /**
+     * 상품문의 답변 등록을 위한 메서드입니다.
+     * 답변 등록 후, 상품문의 상세페이지로 redirect 됩니다.
+     *
+     * @param request        상품문의 답변 등록에 필요한 정보를 담은 dto
+     * @param imagePathList  상품문의 답변에 저장된 이미지 경로 리스트
+     * @param servletRequest 요청
+     * @return 관리자 상품문의 상세페이지
+     */
     @Auth
     @PostMapping("/admin/inquiryForm")
     public String inquirySubmit(@ModelAttribute CreateInquiryRequestDto request,
+                                @RequestParam(value = "imagePathList", required = false)
+                                String imagePathList,
                                 HttpServletRequest servletRequest) {
         Long memberNo = Long.parseLong((String) SecurityContextHolder.getContext()
                 .getAuthentication().getPrincipal());
 
-        inquiryService.submitInquiry(memberNo, request);
+        inquiryService.submitInquiry(memberNo, request, imagePathList);
 
         return "redirect:" + servletRequest.getHeader("Referer");
     }
 
+    /**
+     * 상품문의 답변을 삭제하기 위한 메서드입니다.
+     * 답변 삭제 후, 상품문의 상세페이지로 redirect 됩니다.
+     *
+     * @param inquiryNo      상품문의 번호
+     * @param servletRequest request 요청
+     * @return 관리자 상품문의 상세페이지
+     */
     @Auth
     @PostMapping("/admin/inquiries/{inquiryNo}/cancel")
     public String inquiryAnswerCancel(@PathVariable("inquiryNo") Long inquiryNo,
@@ -49,6 +69,12 @@ public class AdminInquiryController {
         return "redirect:" + servletRequest.getHeader("Referer");
     }
 
+    /**
+     * 상품문의 답변 완료 처리를 위한 메서드입니다.
+     *
+     * @param inquiryNo 답변 완료 여부 수정할 상품문의 번호
+     * @return 상품문의 관리자 페이지
+     */
     @Auth
     @PostMapping("/admin/inquiries/{inquiryNo}/complete")
     public String inquiryComplete(@PathVariable("inquiryNo") Long inquiryNo) {
@@ -57,12 +83,32 @@ public class AdminInquiryController {
         return "redirect:/admin/inquiries";
     }
 
+    /**
+     * 모든 상품문의 조회(불량상품 조회 제외)를 위한 메서드입니다.
+     *
+     * @param pageable       페이징 정보
+     * @param searchKeyFir   검색 조건
+     * @param searchValueFir 검색 값
+     * @param searchKeySec   검색 조건 두번째
+     * @param searchValueSec 검색 값 두번째
+     * @param model          the model
+     * @return 상품문의 관리자 페이지
+     */
     @Auth
     @GetMapping("/admin/inquiries")
     public String inquiryList(@PageableDefault(size = 20) Pageable pageable,
+                              @RequestParam(value = "searchKeyFir", required = false)
+                              String searchKeyFir,
+                              @RequestParam(value = "searchValueFir", required = false)
+                              String searchValueFir,
+                              @RequestParam(value = "searchKeySec", required = false)
+                              String searchKeySec,
+                              @RequestParam(value = "searchValueSec", required = false)
+                              String searchValueSec,
                               Model model) {
         PageResponse<GetInquirySummaryResponseDto> inquiryMemberList =
-                inquiryService.getInquiryList(pageable);
+                inquiryService.getInquiryList(pageable,
+                        searchKeyFir, searchValueFir, searchKeySec, searchValueSec);
 
         model.addAttribute("inquiryList", inquiryMemberList.getContent());
         model.addAttribute("totalPages", inquiryMemberList.getTotalPages());
@@ -74,6 +120,38 @@ public class AdminInquiryController {
         return "admin/inquiry/inquiryIndex";
     }
 
+    /**
+     * 모든 불량상품 문의 조회를 위한 메서드입니다.
+     *
+     * @param pageable 페이징 정보
+     * @param model    the model
+     * @return 불량상품 관리자 페이지
+     */
+    @Auth
+    @GetMapping("/admin/error-inquiries")
+    public String errorInquiryList(@PageableDefault(size = 20) Pageable pageable,
+                                   Model model) {
+        PageResponse<GetInquirySummaryResponseDto> errorInquiryList =
+                inquiryService.getErrorInquiryList(pageable);
+
+        model.addAttribute("errorInquiryList", errorInquiryList.getContent());
+        model.addAttribute("totalPages", errorInquiryList.getTotalPages());
+        model.addAttribute("currentPage", errorInquiryList.getNumber());
+        model.addAttribute("isNext", errorInquiryList.isNext());
+        model.addAttribute("isPrevious", errorInquiryList.isPrevious());
+        model.addAttribute("pageButtonNum", 5);
+
+        return "admin/inquiry/errorInquiryIndex";
+    }
+
+    /**
+     * 상품문의 단건 조회를 위한 메서드입니다.
+     * 상품문의가 비밀글인 경우 사용됩니다.
+     *
+     * @param inquiryNo 상품문의 번호
+     * @param model     the model
+     * @return 관리자 상품문의 단건 조회 페이지
+     */
     @Auth
     @GetMapping("/admin/inquiries/{inquiryNo}/private")
     public String privateInquiryDetails(@PathVariable("inquiryNo") Long inquiryNo,
@@ -83,6 +161,14 @@ public class AdminInquiryController {
         return "admin/inquiry/inquiryDetail";
     }
 
+    /**
+     * 상품문의 단건 조회를 위한 메서드입니다.
+     * 상품문의가 공개글인 경우 사용됩니다.
+     *
+     * @param inquiryNo 상품문의 번호
+     * @param model     the model
+     * @return 관리자 상품문의 단건 조회 페이지
+     */
     @GetMapping("/admin/inquiries/{inquiryNo}")
     public String publicInquiryDetails(@PathVariable("inquiryNo") Long inquiryNo,
                                        Model model) {
--- src/main/java/com/nhnacademy/bookpub/bookpubfront/inquiry/controller/InquiryController.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/inquiry/controller/InquiryController.java
@@ -5,7 +5,6 @@ import com.nhnacademy.bookpub.bookpubfront.inquiry.dto.request.CreateInquiryRequ
 import com.nhnacademy.bookpub.bookpubfront.inquiry.dto.response.GetInquirySummaryMemberResponseDto;
 import com.nhnacademy.bookpub.bookpubfront.inquiry.service.InquiryService;
 import com.nhnacademy.bookpub.bookpubfront.inquirystatecode.service.InquiryStateCodeService;
-import com.nhnacademy.bookpub.bookpubfront.member.service.MemberService;
 import com.nhnacademy.bookpub.bookpubfront.member.util.MemberUtils;
 import com.nhnacademy.bookpub.bookpubfront.utils.PageResponse;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +17,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * 마이페이지의 상품문의를 다루기 위한 컨트롤러입니다.
+ * 마이페이지 및 상품 상세 상품문의를 다루기 위한 컨트롤러입니다.
  *
  * @author : 정유진
  * @since : 1.0
@@ -31,7 +31,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class InquiryController {
     private final InquiryService inquiryService;
     private final InquiryStateCodeService inquiryStateCodeService;
-    private final MemberService memberService;
     private final MemberUtils memberUtils;
 
 
@@ -39,15 +38,16 @@ public class InquiryController {
      * 회원이 상품문의 작성 요청 시 상품 구매 이력이 있는지 검증하기 위한 메서드입니다.
      * 검증이 완료되면 상품문의 작성 폼으로 이동하고, 실패하면 주문목록으로 redirect 됩니다.
      *
-     * @param productNo the product no
+     * @param productNo 상품번호
      * @param model     the model
-     * @return the string
+     * @return 성공 시, 상품문의 작성 폼 페이지. 실패 시, 주문목록 페이지로 이동
      */
     @Auth
     @GetMapping("/members/inquiryForm/products/{productNo}")
     public String inquiryForm(@PathVariable("productNo") Long productNo, Model model) {
+        boolean verify;
         Long memberNo = memberUtils.getMemberNo();
-        boolean verify = inquiryService.verifyPurchaseProduct(memberNo, productNo);
+        verify = inquiryService.verifyPurchaseProduct(memberNo, productNo);
 
         memberUtils.modelRequestMemberNo(model);
         model.addAttribute("inquiryStateCodeList",
@@ -64,23 +64,27 @@ public class InquiryController {
      * 상품문의 작성 폼을 제출했을 시 실행되는 메서드입니다.
      * 해당 회원의 마이페이지 상품문의 내역 페이지로 redirect 됩니다.
      *
-     * @param request the request
-     * @return the string
+     * @param request    상품문의 등록을 위해 필요한 정보를 담은 dto
+     * @param imagePaths 상품문의에 같이 등록될 이미지 경로
+     * @return 마이페이지의 상품문의 페이지
      */
     @Auth
     @PostMapping("/members/inquiryForm")
-    public String inquirySubmit(@ModelAttribute CreateInquiryRequestDto request) {
+    public String inquirySubmit(@ModelAttribute CreateInquiryRequestDto request,
+                                @RequestParam(value = "imagePathList", required = false)
+                                String imagePaths) {
         Long memberNo = memberUtils.getMemberNo();
-        inquiryService.submitInquiry(memberNo, request);
+        inquiryService.submitInquiry(memberNo, request, imagePaths);
 
-        return "redirect:/members/" + memberNo + "/inquiries";
+        return "redirect:/members/inquiries";
     }
 
     /**
      * 해당 회원의 마이페이지 상품문의 내역 페이지로 이동하는 메서드입니다.
      *
+     * @param pageable 페이징 정보
      * @param model    the model
-     * @return the string
+     * @return 마이페이지 상품문의 페이지
      */
     @Auth
     @GetMapping("/members/inquiries")
@@ -101,6 +105,28 @@ public class InquiryController {
         return "mypage/myPageInquiryList";
     }
 
+    /**
+     * 상품문의를 삭제하기 위한 메서드입니다.
+     *
+     * @param inquiryNo 삭제할 상품문의 번호
+     * @return 마이페이지의 상품문의 페이지
+     */
+    @Auth
+    @PostMapping("/members/inquiries/{inquiryNo}/cancel")
+    public String inquiryCancel(@PathVariable Long inquiryNo) {
+        inquiryService.cancelInquiry(inquiryNo);
+
+        return "redirect:/members/inquiries";
+    }
+
+    /**
+     * 마이페이지의 상품문의 상세조회를 위한 메서드입니다.
+     * 비공개 문의글인 경우 사용됩니다.
+     *
+     * @param inquiryNo 상품문의 번호
+     * @param model     the model
+     * @return 마이페이지 상품문의 상세조회 뷰
+     */
     @Auth
     @GetMapping("/members/inquiries/{inquiryNo}/private")
     public String myPagePrivateInquiryDetails(@PathVariable("inquiryNo") Long inquiryNo,
@@ -111,6 +137,14 @@ public class InquiryController {
         return "mypage/myPageInquiryDetail";
     }
 
+    /**
+     * 마이페이지의 상품문의 상세조회를 위한 메서드입니다.
+     * 공개 문의글인 경우 사용됩니다.
+     *
+     * @param inquiryNo 상품문의 번호
+     * @param model     the model
+     * @return 마이페이지 상품문의 상세조회 뷰
+     */
     @GetMapping("/members/inquiries/{inquiryNo}")
     public String myPagePublicInquiryDetails(@PathVariable("inquiryNo") Long inquiryNo,
                                              Model model) {
