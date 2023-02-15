3b71abf4a2c496216ee433fba48a279e30f9a35e
limtaewon <tagkdj1@gmail.com>
2023-02-15 17:35:41 +0900

    [Refactor] 페이지네이션 유틸 생성 및 예시

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/order/service/impl/OrderServiceImpl.java
@@ -56,13 +56,14 @@ public class OrderServiceImpl implements OrderService {
         Map<Long, Long> productAmountMap = new HashMap<>();
         Map<Long, Long> productCoupon = new HashMap<>();
         Map<Long, Long> productSaleAmount = new HashMap<>();
+        Map<Long, Long> productPointSave = new HashMap<>();
 
         List<String> productInfoList
                 = Arrays.stream(productInfo.split(",")).collect(Collectors.toList());
 
         for (String productCouponInfo : productInfoList) {
             parsingAndInput(productNos, productCount, productAmountMap,
-                    productCoupon, productSaleAmount, productCouponInfo);
+                    productCoupon, productSaleAmount, productPointSave, productCouponInfo);
         }
 
         CreateOrderRequestDto createOrderRequestDto = CreateOrderRequestDto.builder()
@@ -73,6 +74,7 @@ public class OrderServiceImpl implements OrderService {
                 .productAmount(productAmountMap)
                 .productCoupon(productCoupon)
                 .productSaleAmount(productSaleAmount)
+                .productPointSave(productPointSave)
                 .build();
 
         return orderAdaptor.createOrderRequest(createOrderRequestDto);
@@ -83,6 +85,7 @@ public class OrderServiceImpl implements OrderService {
                                         Map<Long, Long> productAmountMap,
                                         Map<Long, Long> productCoupon,
                                         Map<Long, Long> productSaleAmount,
+                                        Map<Long, Long> productPointSave,
                                         String productCouponInfo) {
         String productSection = productCouponInfo.split(PRODUCT_COUPON_DIVIDER)[0];
         String couponSection = productCouponInfo.split(PRODUCT_COUPON_DIVIDER)[1];
@@ -91,10 +94,12 @@ public class OrderServiceImpl implements OrderService {
         Long productNo = Long.parseLong(productInfo[0]);
         Integer productCnt = Integer.parseInt(productInfo[1]);
         Long productAmount = Long.parseLong(productInfo[2]);
+        Long pointSave = Long.parseLong(productInfo[3]);
 
         productNos.add(productNo);
         productCount.put(productNo, productCnt);
         productAmountMap.put(productNo, productAmount);
+        productPointSave.put(productNo, pointSave);
 
         if (!couponSection.equals(" ")) {
             couponSection = couponSection.trim();
@@ -178,6 +183,14 @@ public class OrderServiceImpl implements OrderService {
     public PageResponse<GetProductByCategoryResponseDto> getEbooksByMember(
             Pageable pageable, Long memberNo) {
         return orderAdaptor.getEbooksByMember(pageable, memberNo);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void confirmOrderProduct(String orderProductNo, Long memberNo) {
+        orderAdaptor.confirmOrderProduct(orderProductNo, memberNo);
     }
 
 
