8485ae3eb661196ed28d88fae79b4ad676d6353f
Gyeongseo Bak <rudtj2966@gmail.com>
2023-02-19 17:29:24 +0900

    [Refactor] 통합 검색 추가 구현

--- src/main/java/com/nhnacademy/bookpub/bookpubfront/elastic/controller/SearchController.java
+++ src/main/java/com/nhnacademy/bookpub/bookpubfront/elastic/controller/SearchController.java
@@ -1,9 +1,12 @@
 package com.nhnacademy.bookpub.bookpubfront.elastic.controller;
 
 import com.nhnacademy.bookpub.bookpubfront.category.util.CategoryUtils;
+import com.nhnacademy.bookpub.bookpubfront.elastic.dto.response.AllSearchResponseDto;
 import com.nhnacademy.bookpub.bookpubfront.elastic.dto.response.ProductSearchResultDto;
 import com.nhnacademy.bookpub.bookpubfront.elastic.service.ElasticService;
+import com.nhnacademy.bookpub.bookpubfront.state.SearchState;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,9 +39,20 @@ public class SearchController {
     @GetMapping("/search")
     public String mainSearch(@RequestParam(name = "type") String type,
                              @RequestParam(name = "q") String keyword, Model model) {
+        if (type.equals("0")) {
+            Map<String, List<AllSearchResponseDto>> searchAll = searchService.searchAll(keyword);
 
-        List<ProductSearchResultDto> products = searchService.searchProduct(keyword);
-        model.addAttribute("products", products);
+            List<AllSearchResponseDto> productResults = searchAll.get(SearchState.PRODUCT.getKey());
+            List<AllSearchResponseDto> faqResults = searchAll.get(SearchState.FAQ.getKey());
+            List<AllSearchResponseDto> noticeResults = searchAll.get(SearchState.NOTICE.getKey());
+
+            model.addAttribute("productResults", productResults);
+            model.addAttribute("faqResults", faqResults);
+            model.addAttribute("noticeResults", noticeResults);
+        } else if (type.equals("1")) {
+            List<ProductSearchResultDto> products = searchService.searchProduct(keyword);
+            model.addAttribute("products", products);
+        }
 
         categoryUtils.categoriesView(model);
         model.addAttribute("keyword", keyword);
