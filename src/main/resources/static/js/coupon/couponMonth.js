d2c319ffcd262ca1409060f420b6d7d28cf01744
seohyeon <kgustjgus@naver.com>
2023-02-21 20:41:57 +0900

    [Design] : 이달의 쿠폰 mq 삽입

--- src/main/resources/static/js/coupon/couponMonth.js
+++ src/main/resources/static/js/coupon/couponMonth.js
@@ -1,5 +1,7 @@
+let interval;
+let callCount = 0;
+const memberNo = $('#memberNo').text();
 function clickInfo(templateNo) {
-    const memberNo = $('#memberNo').text();
 
     var openedAt = document.getElementById(templateNo).textContent;
 
@@ -25,37 +27,43 @@ function clickInfo(templateNo) {
             url: "/coupon/month-coupon",
             data: {"memberNo": memberNo, "templateNo": templateNo},
             success: function (result) {
-                if (result === 3) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: '쿠폰이 발급되었습니다!',
-                        text: '마이 쿠폰함을 확인해주세요.'
-                    })
-                } else if (result === 2) {
-                    Swal.fire({
-                        icon: 'warning',
-                        title: '쿠폰이 소진되었습니다.',
-                        text: '다음 기회에 참여해주세요.'
-                    })
-
-                } else if (result === 1) {
-                    Swal.fire({
-                        icon: 'warning',
-                        title: '이미 수령한 쿠폰입니다.',
-                        text: '마이 쿠폰함을 확인해주세요.'
-                    })
-                } else {
-                    Swal.fire({
-                        icon: 'warning',
-                        title: '오류가 발생하였습니다..',
-                        text: '다시 시도해주세요.'
-                    })
-
+                if(result === "OK"){
+                    interval = setInterval(function () { confirm(templateNo); },5000);
                 }
             }
-
         })
     }
+}
+
+function confirm(templateNo){
+    callCount++;
+    $.ajax({
+        type:"get",
+        url:"/coupon/month-coupon/check-issued",
+        data: {"memberNo": memberNo, "templateNo": templateNo},
+        success: function (result){
+            if(callCount == 3){
+                console.log("call count 초과");
+                clearInterval(interval);
+                Swal.fire({
+                    icon: 'warning',
+                    title: '시간이 초과되었습니다..',
+                    text: '다시 시도해주세요.'
+                })
+            }
+            else if(result === true){
+                console.log("true 반환");
+                clearInterval(interval);
+                Swal.fire({
+                    icon: 'success',
+                    title: '쿠폰이 발급되었습니다!',
+                    text: '마이 쿠폰함을 확인해주세요.'
+                })
+            }
+        }
+
+    })
+
 }
 
 
